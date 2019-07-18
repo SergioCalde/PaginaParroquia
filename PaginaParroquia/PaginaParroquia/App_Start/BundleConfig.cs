13dada653c178d13d550b4c36f31e8a5cb33dca1
Sergio <sergio.calderon.dm@outlook.com>
2019-07-17 22:07:47 -0600

    Agregar campo de fecha para agregar personas

--- PaginaParroquia/PaginaParroquia/App_Start/BundleConfig.cs
+++ PaginaParroquia/PaginaParroquia/App_Start/BundleConfig.cs
@@ -25,6 +25,17 @@ namespace PaginaParroquia
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //Create bundel for jQueryUI  
+            //js  
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                      "~/Scripts/jquery-ui-1.12.1.js"));
+            //css  
+            bundles.Add(new StyleBundle("~/Content/cssjqryUi").Include(
+                   "~/Content/jquery-ui.css"));
+
+
+
         }
     }
 }
